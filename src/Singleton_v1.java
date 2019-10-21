41c2a263d552a9b4a32384eb56ba901533099bc9
krzysztof.rekus <pik0achu>
2019-10-21 15:54:21 +0200

    Wersja 2

---
+++ src/Singleton_v1.java
@@ -0,0 +1,25 @@
+
+/*
+  1. prywatny konstruktor
+
+ */
+
+
+
+public class Singleton_v1 {
+
+        private static Singleton_v1 ourInstance = new Singleton_v1();
+
+        private int number =314;
+
+        public static Singleton_v1 getInstance() {
+            return ourInstance;
+        }
+
+        private Singleton_v1() {
+        }
+
+    public int getNumber() {
+        return number;
+    }
+}
