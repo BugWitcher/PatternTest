1366f1cc2d1b4358fc98094e562a092e8033c6ec
krzysztof.rekus <pik0achu>
2019-10-21 15:57:00 +0200

    Wersja 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Pattern Test 1.0");
+
+
         System.out.println("Welcome to Pattern Test 2.0");
+
+        Singleton_v1 rex =Singleton_v1.getInstance();
+        System.out.println(rex.getNumber());
+
+        System.out.println("Welcome to Pattern Test 3.0 test");
     }
 }
