e4494f9dd545f63249cd4e12f69dfb5a3013545d
krzysztof.rekus <pik0achu>
2019-10-21 12:07:53 +0200

    Wersja 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Pattern Test 1.0");
+        System.out.println("Welcome to Pattern Test 2.0");
     }
 }
